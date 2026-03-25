b0fab25e597df4b34fb5c9f11879ab1d7062db4f
ShuYjia <yangjiashu0105@outlook.com>
2026-03-25 22:38:48 +0800

    3.25

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject test1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(test1.transform.position);
+    }
+}
